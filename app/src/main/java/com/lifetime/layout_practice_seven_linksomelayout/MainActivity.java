67d91e10338432c8959487afc0e2cd9ccc329d1e
sonminh162 <32251649+sonminh162@users.noreply.github.com>
2019-07-26 09:38:40 +0700

    'first-commit'

---
+++ app/src/main/java/com/lifetime/layout_practice_seven_linksomelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lifetime.layout_practice_seven_linksomelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
